6a46d8ac3f923bf3dc41ceaa40bab4e0bdc417ae
yehontan <yoni333@gmail.com>
2018-06-24 01:35:52 +0300

    more knoledge on modules

--- js-es06/module/main.js
+++ js-es06/module/main.js
@@ -1,7 +1,19 @@
-import { square, diag } from './math.js';
+import {
+    square,
+    diag,
+    callInnerVar
+} from './math.js';
 
 
 addDiv('from outer script : ' + square(12));
 addDiv('from outer script : ' + diag(5, 11));
 console.log(square(12)); // 121
 console.log(diag(5, 10)); // 5
+
+try {
+    console.log(innerModule); //i am private so i will throw error
+} catch (e) {
+    console.log(e);
+}
+
+callInnerVar();
--- js-es06/module/math.js
+++ js-es06/module/math.js
@@ -1,7 +1,15 @@
+console.log('math module here')
+var innerModule = "i am private";
 export const sqrt = Math.sqrt;
 export function square(x) {
     return x * x;
 }
 export function diag(x, y) {
     return sqrt(square(x) + square(y));
+}
+
+export function callInnerVar(){
+    console.log(innerModule + '-- but with export can uncover me just like regular getter');;
+    
+
 }
