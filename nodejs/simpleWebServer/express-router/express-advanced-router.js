639a271c7b9cd9fd5bfef9ba7637f1fb1e4e7687
yehontan <yoni333@gmail.com>
2018-08-28 11:55:02 +0300

    advanced router for nodejs

--- nodejs/simpleWebServer/express-router/express-advanced-router.js
+++ nodejs/simpleWebServer/express-router/express-advanced-router.js
@@ -1,7 +1,16 @@
-
+//new commands
+//routerEvents = express.Router();   
+//routerEvents.use(function (req, res, next) {
+    //next()
+//}
+//app.use('/calander', routerEvents);
+//**************** */
 var data = require('./data')
 console.log('data', data)
 var express = require('express');
+
+var path =require('path')
+var url =require('url')
 const port = 5500;
 var app = express();
 
@@ -40,19 +49,48 @@ routerProjects.use(function (req, res, next) {
 
 // will handle any request that ends in /events
 // depends on where the router is "use()'d"
-routerProjects.get('/single/:id', function (req, res, next) {
+routerProjects.get('/project/:id', function (req, res, next) {
     let id = req.params.id;
     let answer = data.projects.filter(project=>project.id=== parseInt(id))
    res.send(answer)
 });
 
-routerProjects.get('/all', function (req, res, next) {
-    let id = req.params.id;
+routerProjects.get('/projects', function (req, res, next) {
+   
     let answer = data.projects
    res.send(answer)
 });
+
 // only requests to /calendar/* will be sent to our "router"
 app.use('/calander', routerEvents);
 app.use('/projects', routerProjects);
 app.use('/api/v1/projects',routerProjects)
-app.listen(port)
+
+app.get("/url-path-example",function(req,res){
+    let answer = `
+    req.url = ${req.url}  |
+    url.parse(req.url) = ${url.parse(req.url)} |
+    url.parse(req.url).pathname; ${url.parse(req.url).pathname} |
+    process.cwd()= ${process.cwd()} |
+    __dirname  =   ${__dirname} |
+    unescape(url.parse(req.url).pathname) ${unescape(url.parse(req.url).pathname)}  |
+    path.join ( process.cwd() ,  unescape( url.parse(req.url).pathname; ) ); =  ${path.join ( process.cwd() ,  unescape( url.parse(req.url).pathname ) )} |
+
+   
+
+    `
+   res.send(answer)
+
+})
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname +'/public/') //send index.html
+})
+
+app.get("/node_modules/axios/dist/axios.js",(req,res)=>{
+    res.sendFile(__dirname +'/public/node_modules/axios/dist/axios.js') //send index.html
+})
+
+app.listen(port);
+
+console.log('listen on port ' + port);
